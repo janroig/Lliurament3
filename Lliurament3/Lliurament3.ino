01dbf1e5de226c071158e681f02dfbaded9987e3
janroig <jancore10@gmail.com>
2016-02-05 19:29:50 +0100

    acabat

--- Lliurament3/Lliurament3.ino
+++ Lliurament3/Lliurament3.ino
@@ -10,12 +10,13 @@
 
 //******  Variables  *******************************************
 
+int ledpin=13;
 
 //******  Setup  ***********************************************
 
                           // S'executa una vegada quan el programa s'inicia
 void setup() {            //obre bloc de codi
-  pinMode(13, OUTPUT);    //Posa el pin 13 com a sortida
+  pinMode(ledpin, OUTPUT);    //Posa el pin 13 com a sortida
 }                         //tenca bloc de codi
 
 
@@ -23,8 +24,8 @@ void setup() {            //obre bloc de codi
 
 
   void loop() {             //obre bloc de codi
-  digitalWrite(13, HIGH);   // engega el LED (HIGH es el nivell de tensió)
+  digitalWrite(ledpin, HIGH);   // engega el LED (HIGH es el nivell de tensió)
   delay(1000);              // espera 1 segon
-  digitalWrite(13, LOW);    // apaga el LED disminuint la tensió
+  digitalWrite(ledpin, LOW);    // apaga el LED disminuint la tensió
   delay(1000);              // espera 1 segon
 }
